8f7e281682b37124d211e0472fa28c7874884099
joshnsaunders <joshnsaunders@gmail.com>
2017-01-16 17:07:27 -0700

    Add files via upload

---
+++ Intro to Java/GClass2/simple-js-master/main.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+
+
+// Use JS to add header text into 'main-header' without button click.
+document.getElementById("main-header").innerHTML = "<h1>JS Practice</h1>";
+
+
+
+// Use JS to add paragraph text into 'action1-message' with button click from 'action1-btn'. -->
+document.getElementById("action1-btn").addEventListener("click", function(){
+    document.getElementById("action1-message").innerHTML = "Action #1 Button was Clicked!";
+});
+
+
+
+// Use JS to add an h2 into 'heading2' with button click.
+// The HTML has been created, this is your chance to add JS.
+document.getElementById("header2-btn").addEventListener("click", function(){
+  document.getElementById("header2-message").innerHTML = "Button 2 click";
+});
+
+
+
+// Create 3 examples of JS being added to the HTML page immediately.
+// You'll need to create both the HTML and JS for this.
+document.getElementById("pats").innerHTML = "Pats beat the Texans";
+document.getElementById("chiefs").innerHTML = "Chiefs beat the Steelers";
+document.getElementById("falcons").innerHTML = "Falcons beat the Seahawks";
+
+
+
+
+
+// Create 3 examples of JS being added to the HTML page based on button clicks.
+// You'll need to create both the HTML and JS for this.
